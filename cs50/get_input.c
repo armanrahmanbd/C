4c127f2509a70903d20dc1fffc6a018ca7be3012
armanrahmanbd <armanrahmanbd7@gmail.com>
2026-01-28 15:55:15 +0000

    Get updated input handling in get_input.c

--- cs50/get_input.c
+++ cs50/get_input.c
@@ -8,5 +8,12 @@ int main(void)
   scanf("%s", name);
   // Greet the user
   printf("Hello, %s!\n", name);
+  // Prompt user for their age
+  int age;
+  printf("How old are you? ");
+  scanf("%d", &age);
+  // Display user's age
+  printf("You are %d years old.\n", age);
+  
   return 0;
 }
